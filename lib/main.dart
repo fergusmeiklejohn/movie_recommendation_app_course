a8312e25655c5d920c830593c78d659f6da8747f
Fergus Meiklejohn <fergusmeiklejohn@gmail.com>
2021-09-28 15:12:10 +0800

    lesson 3 Flow of Pages

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:movie_recommendation_app_course/features/movie_flow/movie_flow.dart';
 import 'package:movie_recommendation_app_course/theme/custom_theme.dart';
 
 void main() {
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
       title: 'Movie Recommendation',
       darkTheme: CustomTheme.darkTheme(context),
       themeMode: ThemeMode.dark,
-      home: const HomeScreen(),
+      home: const MovieFlow(),
     );
   }
 }
